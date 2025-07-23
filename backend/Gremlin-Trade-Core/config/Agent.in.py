d9d6d9a0b93a941a69e456fe2b7aa4b9bb4fcbf2
Statik DK Smoke <ascend.gremlin@gmail.com>
2025-07-23 00:31:45 -0500

    Create Agent.in.py

---
+++ backend/Gremlin-Trade-Core/config/Agent.in.py
@@ -0,0 +1,3 @@
+#Send logs to Agent.out for loging and send all things memory to the embedder.
+
+#Fully analyze all scripts, and intended purposes from the architecture, use centealized imports for everything through the globals.py
